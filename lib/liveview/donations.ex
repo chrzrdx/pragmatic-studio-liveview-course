3c020ad6935cb7eee6b81230180bac177ecd3213
Abhinandan Panigrahi <chrzrdx@gmail.com>
2025-03-16 16:40:54 -0600

    Add sortable table

--- lib/liveview/donations.ex
+++ lib/liveview/donations.ex
@@ -2,11 +2,33 @@ defmodule Liveview.Donations do
   @moduledoc """
   The Donations context.
   """
-
   import Ecto.Query, warn: false
+  import Ecto.Changeset
   alias Liveview.Repo
 
   alias Liveview.Donations.Donation
+
+  defmodule ListParams do
+    use Ecto.Schema
+    import Ecto.Changeset
+
+    @primary_key false
+    embedded_schema do
+      field :sort_by, :string, default: "id"
+      field :sort_direction, :string, default: "asc"
+      field :page, :integer, default: 1
+      field :max_per_page, :integer, default: 10
+    end
+
+    def changeset(params) do
+      %__MODULE__{}
+      |> cast(params, [:sort_by, :sort_direction, :page, :max_per_page])
+      |> validate_inclusion(:sort_by, ~w(id name quantity days_until_expires))
+      |> validate_inclusion(:sort_direction, ~w(asc desc))
+      |> validate_number(:page, greater_than: 0)
+      |> validate_number(:max_per_page, greater_than: 0, less_than_or_equal_to: 100)
+    end
+  end
 
   @doc """
   Returns the list of donations.
@@ -17,8 +39,37 @@ defmodule Liveview.Donations do
       [%Donation{}, ...]
 
   """
-  def list_donations do
-    Repo.all(Donation)
+  def list_donations(params \\ %{}) when is_map(params) do
+    params = validate_list_params(params)
+
+    Donation
+    |> sort(params)
+    |> paginate(params)
+    |> Repo.all()
+  end
+
+  def validate_list_params(params) do
+    case apply_action(ListParams.changeset(params), :validate) do
+      {:ok, params} ->
+        params |> Map.from_struct()
+
+      {:error, _changeset} ->
+        %ListParams{} |> Map.from_struct()
+    end
+  end
+
+  defp sort(query, %{sort_by: sort_by, sort_direction: sort_direction}) do
+    sort_direction = String.to_atom(sort_direction)
+    sort_by = String.to_atom(sort_by)
+
+    query
+    |> order_by([{^sort_direction, ^sort_by}])
+  end
+
+  defp paginate(query, %{page: page, max_per_page: max_per_page}) do
+    query
+    |> limit(^max_per_page)
+    |> offset(^((page - 1) * max_per_page))
   end
 
   @doc """
--- lib/liveview_web/controllers/donations_live.ex
+++ lib/liveview_web/controllers/donations_live.ex
@@ -4,19 +4,30 @@ defmodule LiveviewWeb.DonationsLive do
 
   @impl true
   def mount(_params, _session, socket) do
-    donations = Donations.list_donations() |> IO.inspect(label: "DONATIONS")
-    {:ok, assign(socket, donations: donations)}
+    {:ok, socket}
   end
 
-  # def handle_params(params, _uri, socket) do
-  #   {:noreply, socket}
-  # end
+  @impl true
+  def handle_params(params, _uri, socket) do
+    params = Donations.validate_list_params(params)
+    donations = Donations.list_donations(params)
+
+    {:noreply, assign(socket, donations: donations, params: params)}
+  end
 
   defp sort_icon(assigns) do
     ~H"""
-    <!-- Active: "bg-gray-200 text-gray-900 group-hover:bg-gray-300", Not Active: "invisible text-gray-400 group-hover:visible group-focus:visible" -->
-    <span class="invisible ml-2 flex-none rounded-sm text-gray-400 group-hover:visible group-focus:visible">
-      <svg class="size-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
+    <span class={[
+      "invisible ml-2 flex-none rounded-sm text-white group-hover:visible group-focus:visible",
+      "group-data-sort-direction:visible group-data-sort-direction:bg-indigo-500"
+    ]}>
+      <svg
+        class="size-5 transition-transform duration-300 ease-in-out group-data-[sort-direction=asc]:rotate-180"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+        aria-hidden="true"
+        data-slot="icon"
+      >
         <path
           fill-rule="evenodd"
           d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z"
@@ -26,4 +37,8 @@ defmodule LiveviewWeb.DonationsLive do
     </span>
     """
   end
+
+  defp get_next_sort_order("asc"), do: "desc"
+  defp get_next_sort_order("desc"), do: "asc"
+  defp get_next_sort_order(_), do: "asc"
 end
