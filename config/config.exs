5375d7153b4a1fe7e6ff77a409c0e73af03f6707
Abhinandan Panigrahi <chrzrdx@gmail.com>
2025-03-28 20:56:01 -0600

    Update deps

--- config/config.exs
+++ config/config.exs
@@ -43,7 +43,7 @@ config :esbuild,
 
 # Configure tailwind (the version is required)
 config :tailwind,
-  version: "4.0.12",
+  version: "4.0.17",
   liveview: [
     args: ~w(
       --input=assets/css/app.css
